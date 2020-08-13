4fa767658da92306357349b207e8ae0140239640
HarryKay <kay.harry.j@gmail.com>
2020-08-13 15:37:10 -0400

    Updated with corrections

--- Missions-To-Mars/scrape_mars.py
+++ Missions-To-Mars/scrape_mars.py
@@ -8,7 +8,7 @@ from splinter import Browser
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    executable_path = {"executable_path": "C:/Users/powde/Downloads/chromedriver.exe"}
     return Browser("chrome", **executable_path, headless=False)
 
 
